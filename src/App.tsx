9300a2eb6c234e8e0f68dfc8298368d83137a01b
Thoretton Edwin <thoretton.edwin@gmail.com>
2023-02-24 21:19:46 +0100

    adding some UI elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
+import SpeedDial from '@mui/material/SpeedDial';
 import React from 'react';
-import CustomAppBar from './CustomAppBar';
+import MicIcon from '@mui/icons-material/Mic';
+import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 export default function () {
   return (
     <>
       <CustomAppBar />
-      <div>Hello world !</div>
+      <Typography
+        variant="body1"
+        component="div"
+        sx={{ flexGrow: 1, textAlign: 'center', marginTop: '16rem' }}
+      >
+        Please use the bottom right button to start recording and get in tune !
+      </Typography>
+      <RecordButton />
     </>
   );
 }
+
+function CustomAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Chromatic Tuner
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+function RecordButton() {
+  return (
+    <SpeedDial
+      ariaLabel="recording action"
+      sx={{ position: 'absolute', bottom: 16, right: 16 }}
+      icon={<MicIcon />}
+      onClick={startRecording}
+    />
+  );
+}
+
+function startRecording() {}
