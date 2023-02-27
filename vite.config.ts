7c7d267946c452ffd10826bc0f702ef3b89a6603
Thoretton Edwin <thoretton.edwin@gmail.com>
2023-02-27 14:04:04 +0100

    fix package lock conflict
    fix conflict bewtween react version for vitePWA and vitest
    
    add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,17 @@
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: '/',
   build: {
     sourcemap: true,
-    assetsDir: "code",
+    assetsDir: 'code',
   },
   plugins: [
     viteTsConfigPaths({
       root: './',
     }),
     reactRefresh(),
-    VitePWA({
-      strategies: "injectManifest",
-      injectManifest: {
-        swSrc: 'public/sw.js',
-        swDest: 'dist/sw.js',
-        globDirectory: 'dist',
-        globPatterns: [
-          '**/*.{html,js,css,json, png}',
-        ],
-      },
-      devOptions: {
-        enabled: true
-      }
-    })
-  ]
-})
+  ],
+});
